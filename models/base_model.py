cd5728ec9317be69061c6ddfab5a92eed75aab3e
jzamora5 <jzamora_5@yahoo.com>
2020-02-13 14:51:06 +0000

    FIX Moved Test File to correct folder and added __init__

---
+++ models/base_model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+BaseModel class that defines all common attributes/methods
+for other classes
+
+"""
+
+class BaseModel:
+
+    """ BaseModel Class definition """
+
+    id = ""
+
+    def __init__(self, id=None):

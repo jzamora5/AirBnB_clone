b4d8a54b8df2b5f9fe33f81dd91931befeb7de70
jzamora5 <jzamora_5@yahoo.com>
2020-02-13 19:07:24 +0000

    UPDATE base_model Class

--- models/base_model.py
+++ models/base_model.py
@@ -6,10 +6,58 @@ for other classes
 
 """
 
+from datetime import datetime
+from uuid import uuid4
+
+
 class BaseModel:
 
     """ BaseModel Class definition """
 
-    id = ""
+    id = '0'
+    created_at = "0-0-0T0:0:0.0"
+    updated_at = "0-0-0T0:0:0.0"
 
-    def __init__(self, id=None):
+    def __init__(self, *args, **kwargs):
+        """ Constructor """
+
+        for key, value in kwargs.items():
+            if key == "__class__":
+                continue
+
+            if (key == "created_at" or key == "updated_at"):
+                value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+
+            setattr(self, key, value)
+
+        if "id" not in kwargs.keys():
+            self.id = str(uuid4())
+
+        if "created_at " not in kwargs.keys():
+            self.created_at = datetime.now()
+
+        if "updated_at" not in kwargs.keys():
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """ Defines what should be printed for each instance of the class """
+        st = "[{:s}] ({:s}) {:s}"
+        st = st.format(self.__class__.__name__, self.id, str(self.__dict__))
+        return st
+
+    def save(self):
+        """
+        Update the Public Instance Attr updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values of __dict__
+        of the instance
+        """
+        dcopy = self.__dict__.copy()
+        dcopy["__class__"] = self.__class__.__name__
+        dcopy["created_at"] = self.created_at.isoformat()
+        dcopy["updated_at"] = self.updated_at.isoformat()
+        return dcopy

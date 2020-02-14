a5b53d645a237ce62ed0154376ab859bf2a5c14b
jzamora5 <jzamora_5@yahoo.com>
2020-02-14 01:54:32 +0000

    ADD More Classes for AirBnB Console

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Amenity class that inherits from BaseModel
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ Class that defines attributes for amenity """
+
+    name = ""
